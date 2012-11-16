c4f4b602b13002833fb1bdb0357d0c49ba6a5443
laurenceputra <laurenceputra@gmail.com>
2012-11-16 21:41:52 +0800

    report done

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 mkdir tmp
 mkdir tmp/$1
-pypy mesi_threaded.py WEATHER $1 1024 64 > tmp/$1/1 &
-pypy mesi_threaded.py WEATHER $1 2048 64 > tmp/$1/2 &
-pypy mesi_threaded.py WEATHER $1 4096 64 > tmp/$1/3 &
-pypy mesi_threaded.py WEATHER $1 8192 64 > tmp/$1/4 &
-pypy mesi_threaded.py WEATHER $1 16384 64 > tmp/$1/5 &
-pypy mesi_threaded.py WEATHER $1 32768 64 > tmp/$1/6 &
+pypy mesi_sim.py WEATHER $1 1024 64 > tmp/$1/1 &
+pypy mesi_sim.py WEATHER $1 2048 64 > tmp/$1/2 &
+pypy mesi_sim.py WEATHER $1 4096 64 > tmp/$1/3 
+pypy mesi_sim.py WEATHER $1 8192 64 > tmp/$1/4 &
+pypy mesi_sim.py WEATHER $1 16384 64 > tmp/$1/5 &
+pypy mesi_sim.py WEATHER $1 32768 64 > tmp/$1/6 
